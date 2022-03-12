57252b3ffcc5267d46c859752edfbc898a784a4e
Rashad Kokash <richardeo112@gmail.com>
2022-03-12 19:24:26 +0200

    Chore: clean README.md

--- apps/feed-stein/src/app/app.tsx
+++ apps/feed-stein/src/app/app.tsx
@@ -13,7 +13,7 @@ export const App = () => {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to feed-stein!</h1>
+        <h1>Welcome to feedstein!</h1>
         <img
           width="450"
           src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
